6457c08199783ca61c02d6e5f92d7ceab2cc856c
Harry Pierson <harrypierson@outlook.com>
2019-11-13 15:14:07 -0800

    getSale working

--- scripts/invoke-get-sale.ts
+++ scripts/invoke-get-sale.ts
@@ -5,7 +5,7 @@ const rpcUrl = "http://127.0.0.1:49332";
 
 const param1 = Neon.create.contractParam("String", "getSale");
 const param2 = sc.ContractParam.byteArray(
-    "5d6b23c95739c599f0459c823774b22508c25c1e31a2913694ab67e8c0042e99", 
+    u.reverseHex("e564116aab513b66b41c9175390a7e51f64dae9cc0f25349d696137462a2415c"), 
     null);
 
 async function mainAsync() {
